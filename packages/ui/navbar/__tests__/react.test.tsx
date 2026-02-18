ed50a0f3dba65f2cd8d41be76ae7cd7f2d4bb1eb
Tenn Chio <tenn_chio@icloud.com>
2026-02-18 11:54:49 +0800

    test(navbar): import vi in react test

--- packages/ui/navbar/__tests__/react.test.tsx
+++ packages/ui/navbar/__tests__/react.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { expect, it } from 'vitest';
+import { expect, it, vi } from 'vitest';
 import { Navbar } from '../src/react';
 
 void React;
