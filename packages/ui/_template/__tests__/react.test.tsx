60444f30a9aa0ca185c269eae5c210f18f39ad69
Tenn Chio <tenn_chio@icloud.com>
2026-02-28 15:11:08 +0800

    chore(template): update test script and mini tsconfig

--- packages/ui/_template/__tests__/react.test.tsx
+++ packages/ui/_template/__tests__/react.test.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import { Component } from '../src/react/component';
