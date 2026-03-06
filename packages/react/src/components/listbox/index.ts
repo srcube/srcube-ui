d0bad897b6838a7d940b80389055683f0943c7ac
Tenn Chio <tenn_chio@icloud.com>
2026-03-06 11:46:40 +0800

    fix(react): export listbox types

--- packages/react/src/components/listbox/index.ts
+++ packages/react/src/components/listbox/index.ts
@@ -1,2 +1,3 @@
-export type { ListboxReactProps } from './props';
+export type { ListboxItem, ListboxReactProps } from './props';
 export { Listbox } from './listbox';
+export type { ListboxLocale } from './locale';
